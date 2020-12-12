f1e335ec33de1dbd954424b242795aaeb1ed5433
Mohamed Douffir <mohamed.douffir@gmail.com>
2020-12-12 16:02:53 -0500

    Defined marlers in pytest.ini and apply them to test functions in test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -3,32 +3,57 @@ Unit tests for the calculator library
 """
 import math
 import calculator
+from pytest import mark
 
 
 class TestCalculator:
 
-    def test_addition(self):
+    @mark.add
+    def test_addition1(self):
         assert 4 == calculator.add(2, 2)
 
-    def test_subtraction(self):
+    @mark.add
+    def test_addition2(self):
+        assert 20 == calculator.add(14, 6)
+
+    @mark.sub
+    def test_subtraction1(self):
         assert 2 == calculator.subtract(4, 2)
 
-    def test_multiplication(self):
+    @mark.sub
+    def test_subtraction2(self):
+        assert 6 == calculator.subtract(20, 14)
+
+    @mark.mult
+    def test_multiplication1(self):
         assert 100 == calculator.multiply(10, 10)
 
-    def test_division(self):
+    @mark.mult
+    def test_multiplication2(self):
+        assert 36 == calculator.multiply(9, 4)
+
+    @mark.div
+    def test_division1(self):
         assert 100 == calculator.divide(1000, 10)
 
+    @mark.div
+    def test_division2(self):
+        assert 25 == calculator.divide(125, 5)
+
+    @mark.power
     def test_square_root(self):
         assert 5 == calculator.square_root(25)
 
+    @mark.power
     def test_power(self):
         assert 8 == calculator.power(2, 3)
 
+    @mark.trig
     def test_cosine(self):
         assert 1.0 == calculator.cosine(0)
         assert -1.0 == calculator.cosine(math.pi)
 
+    @mark.trig
     def test_sine(self):
         assert 0.0 == calculator.sine(0)
         assert 1.0 == calculator.sine(math.pi/2)
